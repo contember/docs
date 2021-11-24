1efae045bbf7ba6e2abbb44d6cde2cf430bc9a72
jonasNobile <jonas.nobile@icloud.com>
2021-11-24 17:11:47 +0100

    Add layout components

---
+++ src/components/props/layoutProps.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import { PropsTableRow, PropsTableProps } from '../propsTable'
+
+export const topStart = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="topStart"
+        propType="ReactNode"
+        description="The top start part of the layout."
+    />
+)
+
+export const topEnd = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="topEnd"
+        propType="ReactNode"
+        description="The top end part of the layout."
+    />
+)
+
+export const sideBar = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="sideBar"
+        propType="ReactNode"
+        description="The side bar part of the layout."
+    />
+)
+
+export const mainStart = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="mainStart"
+        propType="ReactNode"
+        description="The main start part of the layout."
+    />
+)
+
+export const actionsCenter = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="actionsCenter"
+        propType="ReactNode"
+        description="The actions center part of the layout."
+    />
+)
+
+export const actionsEnd = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="actionsEnd"
+        propType="ReactNode"
+        description="The actions end part of the layout."
+        />
+)
+
+export const actionsStart = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="actionsStart"
+        propType="ReactNode"
+        description="The actions start part of the layout."
+    />
+)
+
+export const main = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="main"
+        propType="ReactNode"
+        description="The main part of the layout."
+    />
+)
+
+export const mainEnd = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="mainEnd"
+        propType="ReactNode"
+        description="The main end part of the layout."
+    />
+)
+
+export const mainCenter = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="mainCenter"
+        propType="ReactNode"
+        description="The main center part of the layout."
+    />
+)
+
+export const sideBarCenter = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="sideBarCenter"
+        propType="ReactNode"
+        description="The side bar center part of the layout."
+    />
+)
+
+export const sideBarEnd = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="sideBarEnd"
+        propType="ReactNode"
+        description="The side bar end part of the layout."
+    />
+)
+
+export const sideBarStart = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="sideBarStart"
+        propType="ReactNode"
+        description="The side bar start part of the layout."
+    />
+)
+
+export const top = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="top"
+        propType="ReactNode"
+        description="The top part of the layout."
+    />
+)
+
+export const topCenter = (props: PropsTableProps) => (
+    <PropsTableRow
+        {...props}
+        prop="topCenter"
+        propType="ReactNode"
+        description="The top center part of the layout."
+    />
+)
