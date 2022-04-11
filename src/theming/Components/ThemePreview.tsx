35a297107293595a3e0fc318088f85bfd765d8fc
Martin Adamko <attitude@users.noreply.github.com>
2022-04-11 09:19:11 +0200

    Rename too general `Preview` component

---
+++ src/theming/Components/ThemePreview.tsx
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Intent,
+  Scheme,
+  toSchemeClass,
+  toThemeClass
+} from "@contember/ui"
+import classNames from "classnames"
+import * as React from 'react'
+
+export const ThemePreview = ({
+  heading,
+  scheme,
+  theme,
+  themeContent,
+  themeControls,
+}: {
+  heading: string,
+  scheme: Scheme,
+  theme?: Intent,
+  themeContent?: Intent,
+  themeControls?: Intent,
+}) => {
+  const themeClassNames = toThemeClass(themeContent ?? theme, themeContent ?? theme)
+  const schemeClassName = toSchemeClass(scheme)
+
+  return <Box gap="default" className={classNames('theming-theme-preview', schemeClassName, themeClassNames)}>
+    <Heading>{heading}</Heading>
+    {(themeClassNames || schemeClassName) && <p>CSS classes: {classNames(schemeClassName, themeClassNames)}</p>}
+    <Button>Click me!</Button>
+    <Button disabled>Disabled</Button>
+    <Button
+      distinction="primary"
+      intent={themeControls ?? theme}
+    >I am important!</Button>
+  </Box>
+}
--- src/theming/Components/index.ts
+++ src/theming/Components/index.ts
@@ -1,5 +1,5 @@
 export * from './Generator'
-export * from './Preview'
 export * from './Scale'
 export * from './Swatch'
+export * from './ThemePreview'
 export * from './Var'
