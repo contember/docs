88666596f70af511883f206f7f0ddc5b2ecced39
Honza Sládek <me@honzasladek.com>
2022-05-31 10:36:21 +0200

    Datagrid: small update to page + fix some bugs there

--- src/components/props/generalProps.tsx
+++ src/components/props/generalProps.tsx
@@ -35,14 +35,14 @@ export const options = (props: PropsTableRowProps) => (
         {...props}
         prop="options"
         propType={`
-            string | 
-            { 
-                label: ReactNode, value: OptionallyVariableFieldValue, 
-                description: ReactNode, 
-                searchKeywords: string | undefined 
+            string |
+            {
+                label: ReactNode, value: OptionallyVariableFieldValue,
+                description: ReactNode,
+                searchKeywords: string | undefined
             }[]
         `}
-        description="The options for the field. You can use <a href='/admin/data-binding/query-language' target='_blank'>query language</a> to filter the entities."
+        description="The options for the field. You can use <a href='/reference/admin/data-binding/query-language' target='_blank'>query language</a> to filter the entities."
     />
 )
 
@@ -258,7 +258,7 @@ export const entities = (props: PropsTableRowProps) => (
         {...props}
         prop="entities"
         propType="string"
-        description="The name of the entity. You can use <a href='/admin/data-binding/query-language' target='_blank'>query language (Qualified entity list)</a> to filter the entities."
+        description="The name of the entity. You can use <a href='/reference/admin/data-binding/query-language' target='_blank'>query language (Qualified entity list)</a> to filter the entities."
     />
 )
 
@@ -267,7 +267,7 @@ export const entity = (props: PropsTableRowProps) => (
         {...props}
         prop="entity"
         propType="string"
-        description="The name of the entity. You can use <a href='/admin/data-binding/query-language' target='_blank'>query language</a> to filter the entities."
+        description="The name of the entity. You can use <a href='/reference/admin/data-binding/query-language' target='_blank'>query language</a> to filter the entities."
     />
 )
 
